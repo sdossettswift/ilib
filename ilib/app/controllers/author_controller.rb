1f25087aa67f9af74d1a7161756c2ae3a68af9af
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-06-30 01:29:22 -0500

    author controller and routes

--- ilib/app/controllers/author_controller.rb
+++ ilib/app/controllers/author_controller.rb
@@ -1,22 +1,54 @@
 class AuthorController < ApplicationController
   def create
+    @author = author.new
+    @author.name = params[:name][:name]
+    @author.bio = params[:bio][:bio]
+    @author.photo_url = params[:photo_url][:photo_url]
+    if @author.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def new
+    @author = Author.new
+  end
+
+  def show
+    @author = Author.find_by id: params[:id]
   end
 
   def update
+    @author.name = params[:name][:name]
+    @author.bio = params[:bio][:bio]
+    @author.photo_url = params[:photo_url][:photo_url]
+
+    if @author.save
+      redirect_to edit_author_path
+    else
+      render :edit
+    end
   end
 
   def edit
+      @author = Author.find_by id: params[:id]
   end
 
   def delete
+    @author = Author.find_by id: params[:id]
+      @author.destroy
+      redirect_to root_path
+     end
   end
 
   def destroy
-  end
+    @author = Author.find_by id: params[:id]
+      @author.destroy
+      redirect_to root_path
+     end
 
   def index
+    @authors = Author.all.order("title asc")
   end
 end
--- ilib/config/routes.rb
+++ ilib/config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
-  get 'author/create'
-
-  get 'author/new'
-
-  get 'author/update'
-
-  get 'author/edit'
-
-  get 'author/delete'
-
-  get 'author/destroy'
-
-  get 'author/index'
-
-  get 'welcome/index'
-
   root 'books#index'
   get 'books/new' => 'books#new', as: :new_book
   get 'books/:id' => 'books#show', as: :book
@@ -22,4 +6,11 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: :edit_book
   patch 'books/:id' => 'books#update'
 delete 'books/:id' => 'books#delete'
+
+get 'authors/new' => 'authors#new', as: :new_author
+get 'authors/:id' => 'authors#show', as: :author
+post 'authors' => 'authors#create'
+get 'authors/:id/edit' => 'authors#edit', as: :edit_author
+patch 'authors/:id' => 'authors#update'
+delete 'authors/:id' => 'authors#delete'
 end
